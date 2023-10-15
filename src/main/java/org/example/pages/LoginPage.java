f78bfdecacedb30701b5aec2ced64c5e8452bf4d
Hitendra1803 <hitendratajne@gmail.com>
2023-10-15 13:33:47 +0530

    login page in development

--- src/main/java/org/example/pages/LoginPage.java
+++ src/main/java/org/example/pages/LoginPage.java
@@ -1,16 +1,28 @@
 package org.example.pages;
 
 import org.example.base.BaseTest;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.io.IOException;
 
 public class LoginPage extends BaseTest {
     /**
-     * Load and Read Config.Properties File
+     * Page Factory for Login Page
      */
     public LoginPage() throws IOException {
         super();
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(name = "email")
+    WebElement emailField;
+
+    @FindBy(name = "password")
+    WebElement passwordField;
+
+    @FindBy(xpath = "")
+    WebElement emailField;
+
 }
